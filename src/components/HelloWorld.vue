a970038bd001869ee50d76fc70beaa2729b83726
qiqigou <2319002545@qq.com>
2021-11-23 17:39:22 +0800

    create

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+/**
+ * show message
+ * @text context
+ */
+function showMessage(context: string) {
+  alert(context);
+}
+
+showMessage("Di di di !!");
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
